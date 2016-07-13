6559cd52e365ebd1988f9c06c7798b2a48590370
David Ma <David Ma>
2016-07-13 11:23:00 -0700

    Updated analog inputs and constants

--- TestFunctions/ServoTests/ServoTests.ino
+++ TestFunctions/ServoTests/ServoTests.ino
@@ -12,37 +12,48 @@ void setup() {
 void loop() {
   while (startbutton())
   {
-    /*
-      for (int i = 0; i < 180; i++)
-      {
-      RCServo0.write(i);
-      if (RCServo0.read() > 500 && RCServo0.read() <= 1000)
-      {
-        testVal = RCServo0.read();
-        target = i;
-      }
-      delay(7);
-      }
-    */
-    RCServo0.write(0);
-    delay(500);
-    RCServo0.write(180);
-    if (RCServo0.read() >= 1345)
-    {
-      RCServo0.write(90);
-    }
-    LCD.clear();
-    LCD.home();
-    LCD.print(target);
-    LCD.print(" ");
-    LCD.print(testVal);
-    LCD.print(" ");
-    LCD.print(RCServo0.read());
-    LCD.print(" ");
-    LCD.print(map(RCServo0.read(), 450, 1350, 0, 180));
+    intersectionScan(0,950);
   }
   while (stopbutton())
   {
     RCServo0.write(0);
   }
 }
+
+# define INTERSECTION_SCAN_ANGLE_PARTITION 10
+# define INTERSECTION_SCAN_PARTITION_DELAY 50
+/*
+ * function - intersectionScan
+ * 
+ * Scans at an intersection using the specified analog input, if there are signals above the threshold, returns the angle for the highest signal.
+ * If there are no signals above the threshold, returns 0.
+ * 
+ * Assumes the scanning servo is connected to port 0
+ */
+byte intersectionScan(byte analogInputPin, int threshold)
+{
+  LCD.clear();
+  LCD.home();
+  RCServo0.write(0);
+
+  byte bestAngle = 0;
+  int currentVal;
+  int bestVal = 0;
+
+  for (int angle = 1; angle < 180; angle += INTERSECTION_SCAN_ANGLE_PARTITION)
+  {
+    RCServo0.write(angle);
+    currentVal = analogRead(analogInputPin);
+    if (currentVal > threshold && currentVal > bestVal)
+    {
+      bestVal = currentVal;
+      bestAngle = angle; // coold read and convert the pwm from the servo if needed
+    }
+    delay(INTERSECTION_SCAN_PARTITION_DELAY);
+    LCD.clear();
+    LCD.home();
+    LCD.print(RCServo0.read());
+  }
+  return bestAngle;
+}
+
